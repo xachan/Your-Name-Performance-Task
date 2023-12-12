261acc8a0017e31273b744b982fa25b1f376aa67
xachan <121971475+xachan@users.noreply.github.com>
2023-12-12 12:40:58 -0800

    #6

--- Player.gd
+++ Player.gd
@@ -1,9 +1,11 @@
 extends Area2D
 @export var speed = 400 
+@onready var bg_music = $"../BGMusic"
+@onready var enemy_hit = $EnemyHit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	bg_music.play()
  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +31,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	bg_music.stop()
